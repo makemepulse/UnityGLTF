a8c93b187759b75fd08b22d338a219eeb9aeff51
pierre lepers <pierre.lepers@gmail.com>
2020-04-12 18:48:56 +0200

    KHR_materials_pbrSpecularGlossinessExtension refactoring :   - extension object extends GLTFProperty as in the specification   - all properties are optional, Serialisation support null TextureInfos and don't serialize properties with default values

--- GLTFSerialization/GLTFSerialization/Extensions/KHR_materials_pbrSpecularGlossinessExtension.cs
+++ GLTFSerialization/GLTFSerialization/Extensions/KHR_materials_pbrSpecularGlossinessExtension.cs
@@ -12,7 +12,7 @@ namespace GLTF.Schema
 	/// Spec can be found here:
 	/// https://github.com/KhronosGroup/glTF/tree/master/extensions/Khronos/KHR_materials_pbrSpecularGlossiness
 	/// </summary>
-	public class KHR_materials_pbrSpecularGlossinessExtension : IExtension
+	public class KHR_materials_pbrSpecularGlossinessExtension : GLTFProperty, IExtension
 	{
 		public static readonly Vector3 SPEC_FACTOR_DEFAULT = new Vector3(0.2f, 0.2f, 0.2f);
 		public static readonly double GLOSS_FACTOR_DEFAULT = 0.5d; 
@@ -55,7 +55,21 @@ namespace GLTF.Schema
 		/// </summary>
 		public TextureInfo SpecularGlossinessTexture;
 
-		public KHR_materials_pbrSpecularGlossinessExtension(Color diffuseFactor, TextureInfo diffuseTexture, Vector3 specularFactor, double glossinessFactor, TextureInfo specularGlossinessTexture)
+		public KHR_materials_pbrSpecularGlossinessExtension()
+		{
+
+		}
+
+    public KHR_materials_pbrSpecularGlossinessExtension( KHR_materials_pbrSpecularGlossinessExtension ext, GLTFRoot root ) : base( ext, root )
+		{
+			DiffuseFactor = ext.DiffuseFactor;
+			SpecularFactor = ext.SpecularFactor;
+			GlossinessFactor = ext.GlossinessFactor;
+			DiffuseTexture = new TextureInfo( ext.DiffuseTexture, root );
+			SpecularGlossinessTexture = new TextureInfo( ext.SpecularGlossinessTexture, root );
+		}
+
+    public KHR_materials_pbrSpecularGlossinessExtension(Color diffuseFactor, TextureInfo diffuseTexture, Vector3 specularFactor, double glossinessFactor, TextureInfo specularGlossinessTexture)
 		{
 			DiffuseFactor = diffuseFactor;
 			DiffuseTexture = diffuseTexture;
@@ -66,43 +80,88 @@ namespace GLTF.Schema
 
 		public IExtension Clone(GLTFRoot gltfRoot)
 		{
-			return new KHR_materials_pbrSpecularGlossinessExtension(
-				DiffuseFactor,
-				new TextureInfo(
-					DiffuseTexture,
-					gltfRoot
-					), 
-				SpecularFactor,
-				GlossinessFactor,
-				new TextureInfo(
-					SpecularGlossinessTexture,
-					gltfRoot
-					)
-				);
+			return new KHR_materials_pbrSpecularGlossinessExtension( this, gltfRoot );
 		}
+
+		// public JProperty Serialize()
+		// {
+		// 	JProperty jProperty =
+		// 		new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.EXTENSION_NAME,
+		// 			new JObject(
+		// 				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_FACTOR, new JArray(DiffuseFactor.R, DiffuseFactor.G, DiffuseFactor.B, DiffuseFactor.A)),
+		// 				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_TEXTURE,
+		// 					new JObject(
+		// 						new JProperty(TextureInfo.INDEX, DiffuseTexture.Index.Id)
+		// 						)
+		// 					),
+		// 				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_FACTOR, new JArray(SpecularFactor.X, SpecularFactor.Y, SpecularFactor.Z)),
+		// 				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.GLOSSINESS_FACTOR, GlossinessFactor),
+		// 				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_GLOSSINESS_TEXTURE,
+		// 					new JObject(
+		// 						new JProperty(TextureInfo.INDEX, SpecularGlossinessTexture.Index.Id)
+		// 						)
+		// 					)
+		// 				)
+		// 			);
+
+		// 	return jProperty;
+		// }
+
+
+		override public void Serialize(JsonWriter writer)
+		{
+			writer.WriteStartObject();
+
+			if (DiffuseFactor != Color.White)
+			{
+				writer.WritePropertyName(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_FACTOR);
+				writer.WriteStartArray();
+				writer.WriteValue(DiffuseFactor.R);
+				writer.WriteValue(DiffuseFactor.G);
+				writer.WriteValue(DiffuseFactor.B);
+				writer.WriteEndArray();
+			}
+
+			if (SpecularFactor != SPEC_FACTOR_DEFAULT)
+			{
+				writer.WritePropertyName(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_FACTOR);
+				writer.WriteStartArray();
+				writer.WriteValue(SpecularFactor.X);
+				writer.WriteValue(SpecularFactor.Y);
+				writer.WriteValue(SpecularFactor.Z);
+				writer.WriteEndArray();
+			}
+
+			if (GlossinessFactor != GLOSS_FACTOR_DEFAULT)
+			{
+				writer.WritePropertyName(KHR_materials_pbrSpecularGlossinessExtensionFactory.GLOSSINESS_FACTOR);
+				writer.WriteValue(GlossinessFactor);
+			}
+
+			if (DiffuseTexture != null)
+			{
+				writer.WritePropertyName(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_TEXTURE);
+				DiffuseTexture.Serialize( writer );
+			}
+
+			if (SpecularGlossinessTexture != null)
+			{
+				writer.WritePropertyName(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_GLOSSINESS_TEXTURE);
+				SpecularGlossinessTexture.Serialize( writer );
+			}
+
+			base.Serialize(writer);
+
+			writer.WriteEndObject();
+		}
+
 
 		public JProperty Serialize()
 		{
-			JProperty jProperty =
-				new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.EXTENSION_NAME,
-					new JObject(
-						new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_FACTOR, new JArray(DiffuseFactor.R, DiffuseFactor.G, DiffuseFactor.B, DiffuseFactor.A)),
-						new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.DIFFUSE_TEXTURE,
-							new JObject(
-								new JProperty(TextureInfo.INDEX, DiffuseTexture.Index.Id)
-								)
-							),
-						new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_FACTOR, new JArray(SpecularFactor.X, SpecularFactor.Y, SpecularFactor.Z)),
-						new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.GLOSSINESS_FACTOR, GlossinessFactor),
-						new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.SPECULAR_GLOSSINESS_TEXTURE,
-							new JObject(
-								new JProperty(TextureInfo.INDEX, SpecularGlossinessTexture.Index.Id)
-								)
-							)
-						)
-					);
-
+      JProperty jProperty = new JProperty(KHR_materials_pbrSpecularGlossinessExtensionFactory.EXTENSION_NAME);
+      Serialize( jProperty.CreateWriter() );
 			return jProperty;
 		}
+
 	}
 }
